3852d0b3781c5e5b3658acf81fb9aaad549f2f9b
laksherthanlife <102166996+laksherthanlife@users.noreply.github.com>
2026-05-13 17:39:01 +0000

    Deploying to gh-pages from @ laksherthanlife/laksherthanlife.github.io@d9174de056a8ded119f158bc858639c546062129 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -489,14 +489,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Laksh_Resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%61%6D%75%6C%61%6B%73%68%6D%61%6E%61%6E%31%32@%75.%6E%75%73.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -506,25 +506,18 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://github.com/laksherthanlife", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/R-Laksh", "_blank");
         },
       },{
       id: 'light-theme',
